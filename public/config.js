800dfa57bbe469712123552880b47c63d9e2a22b
luoyifan <luoyifan@qq.com>
2020-04-18 13:04:54 +0800

    IE10 兼容

--- public/config.js
+++ public/config.js
@@ -3,20 +3,23 @@ require.config({
     paths: {
         app: '/dist/app',
         tslib: '/static/plugins/tslib/tslib.min',
-        vue: '/static/plugins/vue/vue',
-        axios: '/static/plugins/axios/axios',
-        qs: '/static/plugins/qs/qs',
-        typescript: '/static/plugins/typescript/typescript',
+        vue: '/static/plugins/vue/vue.min',
+        axios: '/static/plugins/axios/axios.min',
+        qs: '/static/plugins/qs/qs.min',
+        typescript: '/static/plugins/typescript/typescript.min',
+        //typescript: 'https://cdn.bootcss.com/typescript/3.7.4/typescript.min',
         webix: '/static/plugins/webix/webix',
         moment: '/static/plugins/moment/moment.min',
+        'polyfill': '/static/plugins/polyfill/polyfill.min',
         'yvan-ui': '/dist2/yvan-ui',
-
     }
 });
 
 $(document).ready(function () {
-    const yvanui = require(['yvan-ui'], function (yvanui) {
+    require(['yvan-ui', 'moment', 'polyfill'], function (yvanui, moment, polyfill) {
         window.YvanUI = yvanui;
+        window.moment = moment;
+        debugger
         require(['app'])
     })
 });
